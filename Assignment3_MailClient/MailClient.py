ed99b689954b0a398129c83c956f398774bcb42d
LAPTOP-CJFJN2SA\ILoveU <592521098@qq.com>
2018-08-23 22:39:34 +0800

    update

--- Assignment3_MailClient/MailClient.py
+++ Assignment3_MailClient/MailClient.py
@@ -9,6 +9,8 @@ sp = ' '
 crlf = '\r\n'
 hello = "HELO"
 auth = "AUTH LOGIN"
+user = "USER"
+passwd = "PASS"
 mailfrom = "MAIL FROM"
 rcptto = "RCPT TO"
 data = "DATA"
@@ -17,51 +19,60 @@ quit = "QUIT"
 ending = '.'
 
 # Mail info
-myUsername = "shengsiyuan"
 myHostname = "ict.ac.cn"
-targetUsername = sys.argv[2]
-targetHostname = sys.argv[3]
-loginName = sys.argv[4]
-loginPasswd = sys.argv[5]
+myMailAddress = "shengsiyuan@ict.ac.cn"
+targetmailAddress = sys.argv[2]
+loginName = sys.argv[3]
+loginPasswd = sys.argv[4]
 
 # Server info
 serverIP = sys.argv[1]
 serverPort = 25
 maxLength = 1024
 
-def bulidCmdBytes(name, value):
+def bulidKVCmdBytes(name, value):
 	return (name + sp + value + crlf).encode('utf-8')
 
-def buildCmdWithColonBytes(name, value):
+def buildCmdBytes(name):
+		return (name + crlf).encode('utf-8')
+
+def buildKVCmdWithColonBytes(name, value):
 	return (name + colon + sp + value + crlf).encode('utf-8')
 
-def buildContactString(name, host):
-    return '<' + name + '@' + host + '>'
+def buildContactString(mailAddress):
+    return '<' + mailAddress + '>'
 
 clientSocket = socket(family=AF_INET, type=SOCK_STREAM)
 clientSocket.connect((serverIP,serverPort))
 
 try:
 	# SMTP handshaking
+	# recv 220 from server
 	recvMessageBytes = clientSocket.recv(maxLength)
 	print(recvMessageBytes.decode('utf-8'))
 
-	clientSocket.send(bulidCmdBytes(hello, myHostname))
+	# hello
+	clientSocket.send(bulidKVCmdBytes(hello, myHostname))
 	recvMessageBytes = clientSocket.recv(maxLength)
 	print(recvMessageBytes.decode('utf-8'))
 
-	clientSocket.send(buildCmdWithColonBytes(mailfrom, buildContactString(myUsername, myHostname)))
-	recvMessageBytes = clientSocket.recv(maxLength)
-	print(recvMessageBytes.decode('utf-8'))
-
+	# auth login
 	clientSocket.send((auth+crlf).encode("utf-8"))
-	# there is an error
-	clientSocket.send(base64.b64encode(loginName+crlf).encode("utf-8"))
-	clientSocket.send(base64.b64encode(loginPasswd+crlf).encode("utf-8"))
+	recvMessageBytes = clientSocket.recv(maxLength)
+	print(recvMessageBytes.decode('utf-8'))
+	
+	clientSocket.send(base64.b64encode(buildCmdBytes(loginName)))
+
+	clientSocket.send(base64.b64encode(buildCmdBytes(loginPasswd)))
+	# recvMessageBytes = clientSocket.recv(maxLength)
+	# print(recvMessageBytes.decode('utf-8'))
+
+	# send from
+	clientSocket.send(buildKVCmdWithColonBytes(mailfrom, buildContactString(myMailAddress)))
 	recvMessageBytes = clientSocket.recv(maxLength)
 	print(recvMessageBytes.decode('utf-8'))
 
-	clientSocket.send(buildCmdWithColonBytes(rcptto, buildContactString(targetUsername, targetHostname)))
+	clientSocket.send(buildKVCmdWithColonBytes(rcptto, buildContactString(targetmailAddress)))
 	recvMessageBytes = clientSocket.recv(maxLength)
 	print(recvMessageBytes.decode('utf-8'))
 
